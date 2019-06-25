6db6cf3f4ca1af1e2dc139e994a3367a90765152
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-06-25 18:46:51 +0200

    Fixes navigation bug.

--- lib/photodetail/PhotoDetailView.dart
+++ lib/photodetail/PhotoDetailView.dart
@@ -1,6 +1,5 @@
-import 'package:digital_nomad_wallpapers/actions/view_state_actions.dart';
+import 'package:digital_nomad_wallpapers/navigation/actions/navigation_actions.dart';
 import 'package:digital_nomad_wallpapers/store/app_state.dart';
-import 'package:digital_nomad_wallpapers/store/navigation_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
@@ -20,13 +19,8 @@ class PhotoDetail extends StatelessWidget {
           ),
           AppBar(
               backgroundColor: Colors.transparent,
-              automaticallyImplyLeading: true,
               title: Text('Hello world'),
-              leading: IconButton(
-                icon: Icon(Icons.arrow_back, color: Colors.white),
-                onPressed: () => store.dispatch(
-                    RouteAction(destinationScreen: PhotoListScreen())),
-              )),
+              automaticallyImplyLeading: true),
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
